f33fe935a5a86f4c673ef50b46f2b8e52bb7582c
lingxiangyang <1150689758@qq.com>
2020-04-01 15:50:07 +0800

    feat: 通用方法

--- src/utils/commonFunc.js
+++ src/utils/commonFunc.js
@@ -84,3 +84,136 @@ export const translateList = (list, mergeKey) => {
     .map((item2, index) => ({ ...item2, rowSpan: obj[tempKey].num, isMerge: index === 0 })));
   return tempArr.reduce((pre, cur) => pre.concat(cur), []);
 };
+
+/**
+ * 数组中的对象根据某字段去重
+ * @param arr 原始数组
+ * @param field 根据该字段去重
+ * @returns {*} 去重后的数组
+ */
+export const uniqueArr = (arr, field) => {
+  const res = new Map();
+  return arr.filter((a) => !res.has(a[field]) && res.set(a[field], 1))
+}
+/**
+ * 保存而禁止内容到指定文件
+ * @param data data
+ * @param fileName fileName
+ */
+export const saveResponseAsBlobFile = function (res) {
+  // console.log(res);
+  let fileName = null;
+  if (res && res.headers) {
+    fileName = res.headers['content-disposition'];
+  }
+  // console.log(fileName);
+  if  (fileName) {
+    const temp =fileName.split('filename=');
+    if (temp && temp.length > 0) {
+      try {
+        fileName = decodeURIComponent(temp[1]);
+      } catch(e) {
+        console.error(e);
+        fileName = temp[1];
+      }
+    } else {
+      fileName = null;
+    }
+  }
+  // console.log(fileName);
+  if (!fileName) {
+    fileName = "unknown";
+  }
+  saveBlobFile(res.data, fileName);
+}
+/**
+ * 保存而禁止内容到指定文件
+ * @param data data
+ * @param fileName fileName
+ */
+export const saveBlobFile = function (data, fileName) {
+  const blob = new Blob([data]);
+  if ("download" in document.createElement("a")) {
+    // 非IE下载
+    const elink = document.createElement("a");
+    elink.download = fileName;
+    elink.style.display = "none";
+    elink.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(elink);
+    elink.click();
+    URL.revokeObjectURL(elink.href); // 释放URL 对象
+    document.body.removeChild(elink);
+  } else {
+    // IE10+下载
+    navigator.msSaveBlob(blob, fileName);
+  }
+}
+
+const fileTypeObj = {
+  zip: 'application/zip',
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8',
+  xls: 'application/vnd.ms-excel',
+  doc: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document;charset=utf-8',
+  docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document;charset=utf-8',
+  txt: 'application/txt',
+  pdf: 'application/pdf',
+};
+
+export const downloadFile = (blobData, fileName, fileType) => {
+  // const blob = new Blob([blobData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8' });
+  const blob = new Blob([blobData], { type: fileTypeObj[fileType] });
+  const downloadElement = document.createElement('a');
+  const href = window.URL.createObjectURL(blob);
+  downloadElement.href = href;
+  downloadElement.download = fileName; // 下载文件名称
+  document.body.appendChild(downloadElement);
+  downloadElement.click();
+  document.body.removeChild(downloadElement); // 下载完成移除元素
+  window.URL.revokeObjectURL(href); // 释放掉blob对象
+};
+
+export const downloadFileByLink = (fileUrl,fileName) => {
+  fetch(`${fileUrl}`).then((res) =>
+    res.blob().then((blob) => {
+      let a = document.createElement('a');
+      let url = window.URL.createObjectURL(blob);
+      let filename = fileName;
+      a.href = url;
+      a.download = filename;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    })
+  );
+};
+
+export const computeHtmlText = (html) => {
+  let divElem = document.createElement('div');
+  divElem.innerHTML = html;
+  return divElem.innerText.length;
+};
+
+// 下划线转换驼峰
+export const toHump = (name) => {
+  return name.replace(/\_(\w)/g, function (all, letter) {
+    return letter.toUpperCase();
+  });
+}
+// 驼峰转换下划线
+export const toLine = (name) => {
+  return name.replace(/([A-Z])/g, "_$1").toLowerCase();
+}
+export const isApp = () => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  const isWeiXin = ua.match(/MicroMessenger/i) == 'micromessenger';
+  const isDingTalk = ua.match(/DingTalk/i)== 'dingtalk';
+  return isWeiXin || isDingTalk;
+}
+
+export const isDingTalk = () => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  return ua.match(/DingTalk/i)== 'dingtalk';
+}
+
+export const isTimeBefore = (time) => {
+  return moment().isBefore(moment(time));
+}
