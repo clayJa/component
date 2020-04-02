51c92bed98534a8edabd3346e036a381ef39b44f
lingxiangyang <1150689758@qq.com>
2020-04-02 10:54:52 +0800

    fix

--- src/components/Form/SelfSwitch.vue
+++ src/components/Form/SelfSwitch.vue
@@ -1,20 +1,21 @@
 <template>
   <van-field name="switch" v-bind="$props" input-align="right">
     <template #input>
-      <van-switch 
-        v-model="localVal" 
-        size="20" 
-        @change="onDataChange" 
-        :active-value="1" 
-        :inactive-value="0" 
-        active-color="#07c160" 
-        inactive-color="#ee0a24" 
+      <van-switch
+        v-model="localVal"
+        size="20"
+        @change="onDataChange"
+        :active-value="1"
+        :inactive-value="0"
+        active-color="#07c160"
+        inactive-color="#ee0a24"
       />
     </template>
   </van-field>
 </template>
 <script>
 import { Field } from 'vant';
+
 export default {
   props: {
     ...Field.props,
@@ -22,9 +23,9 @@ export default {
     options: Array,
   },
   name: 'SelfSwitch',
-  model:{
+  model: {
     prop: 'value',
-    event: 'input'  // 自定义方法，用来更新 model
+    event: 'input', // 自定义方法，用来更新 model
   },
   data() {
     return {
@@ -32,15 +33,15 @@ export default {
     };
   },
   watch: {
-    value(val){
-      this.localVal = val; 
-    }
+    value(val) {
+      this.localVal = val;
+    },
   },
-  methods:{
+  methods: {
     onDataChange(value) {
       this.localVal = value;
-      this.$emit('input',value); // 更新 model
-      this.$emit('self-switch',value);
+      this.$emit('input', value); // 更新 model
+      this.$emit('self-switch', value);
     },
   },
 };
